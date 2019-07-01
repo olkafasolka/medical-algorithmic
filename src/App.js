e3a9abc9ab4f8241c0e553ad4093bb4895386960
olkafasolka <a.placek1@gmail.com>
2019-07-01 19:15:28 +0200

    dodano widok home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
+import Home from './Components/Home'
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-         Bardzo krwawi?
-        </p>
-
-      </header>
+    <div>
+  <Home></Home>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <div><p className="homeHeader">SAVE A LIFE</p>
+
+        </div>
+    )
+}
+
+export default Home;
